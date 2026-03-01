a14309edf4f413ea1eea0bb399f47b9acfdaf98d
subodh-baniya <baniyasubodh07@gmail.com>
2026-03-01 20:47:11 +0545

    web part configured

---
+++ Web/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <div className='flex justify-center items-center h-screen w-screen bg-red-800'>
+      <p className='bg-sky-50 text-red-200 text-center'>hello</p>
+    </div>
+  )
+}
+
+export default App
