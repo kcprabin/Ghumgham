676890a08fcdc516d59ec934b9c5d339bd4d2955
subodh-baniya <baniyasubodh07@gmail.com>
2026-03-02 13:15:20 +0545

    added the hero section in the web

--- Web/Routes/Route.tsx
+++ Web/Routes/Route.tsx
@@ -1,10 +1,11 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import Login from "../Pages/Loginpage"
+import InitialHome from "../Pages/InitialHome"
 
 const router=createBrowserRouter(
     createRoutesFromElements(
         <>
-        <Route path="/" element={"#"}/>
+        <Route path="/" element={<InitialHome/>}/>
         <Route path="/login"element={<Login/>}/>
         <Route path="/register" element={"#"}/>
         </>
