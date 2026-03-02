493002d916fdb073ce7a0d3eea33730843ad7cef
Prabin K.C. <kcprabin2063@gmail.com>
2026-03-02 14:47:21 +0545

    file structured is maintained

---
+++ app/screens/onboarding/splach.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const splach = () => {
+  return (
+    <View>
+      <Text>splach</Text>
+    </View>
+  )
+}
+
+export default splach
